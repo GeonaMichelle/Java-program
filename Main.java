d8a41474545b920d377d6d819beb5683230d246e
GeonaMichelle <nnm24is085@nmamit.in>
2025-10-07 19:44:36 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package source; 
+public class Main { 
+ public static void main(String[] args) { 
+ MessageBoard board = new MessageBoard(); 
+ new Producer(board).start(); 
+ new Consumer(board).start(); 
+ } 
+}
